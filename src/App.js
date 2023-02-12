b4187e849a63d97619003ba3891b2028c34a3580
Shreya Nelakonda <shreyanelakonda@Shreyas-MacBook-Pro.local>
2023-02-12 14:45:20 -0500

    OnlineProductSearch

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import userReducer from './product-app/Reducers/user-reducer';
+import propertyReducer from './product-app/Reducers/properties-reducer';
+import productReducer from "./product-app/Reducers/products-reducer";
+import productByIdReducer from "./product-app/Reducers/product-screen-reducer";
+import categoriesReducer from './product-app/Reducers/categories.reducer';
+import productRequestsReducer from './product-app/Reducers/product-request-reducer.js'
+import reviewsReducer from "./product-app/Reducers/reviews-reducer";
+import productsByCategoryReducer from "./product-app/Reducers/product-category-reducer";
+
+import addressReducer from "./product-app/Reducers/address-reducer";
+import ProductApplication from './product-app';
+
+
+const store = configureStore(
+    {
+        reducer:
+        {
+            user: userReducer,
+            properties: propertyReducer,
+            products: productReducer,
+            users: userReducer,
+            categories: categoriesReducer,
+            productRequests: productRequestsReducer,
+            productById: productByIdReducer,
+            productsByCategory: productsByCategoryReducer,
+            reviews: reviewsReducer,
+            userAddresses: addressReducer
+        }
+    });
+
+function App() {
+    return (
+        <Provider store={store}>
+            <ProductApplication />
+        </Provider>
+    );
+}
+
+export default App;
